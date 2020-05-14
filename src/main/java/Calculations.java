f4429b6377e6d4a61ad7f8ec48816bb2aad1d370
Christian Smit <chrsmit@web.de>
2020-05-14 15:40:17 +0200

    Initial Commit

--- src/main/java/Calculations.java
+++ src/main/java/Calculations.java
@@ -3,4 +3,11 @@ public class Calculations {
     public static int add(int a,int b){
         return a+b;
     }
+
+    public static String coronaCount(int a){
+        if(a<=30){
+            return "OK";
+        }
+        return "LIMIT ÜBERSCHRITTEN!";
+    }
 }
--- src/test/java/CalculationsTest.java
+++ src/test/java/CalculationsTest.java
@@ -1,9 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculationsTest {
     @Test
+    @DisplayName("5 + 3 sollte 8 ergeben")
     public void fivePlus3ShouldBe8(){
         // Given
         int first = 3;
@@ -14,5 +16,31 @@ class CalculationsTest {
 
         // Then
         assertEquals(8, result);
+    }
+
+    @Test
+    @DisplayName("Bei 30 Personen sollte Ergebnis sein: OK")
+    public void coronaShouldntSpreadwith30Ppl(){
+        // Given
+        int a = 30;
+
+        // WHEN
+        String result = Calculations.coronaCount(a);
+
+        // THEN
+        assertEquals("OK", result);
+    }
+
+    @Test
+    @DisplayName("Bei 32 Personen sollte Ergebnis sein: LIMIT ÜBERSCHRITTEN! ")
+    public void coronaMightSpreadwith32Ppl(){
+        // Given
+        int a = 32;
+
+        // WHEN
+        String result = Calculations.coronaCount(a);
+
+        // THEN
+        assertEquals("LIMIT ÜBERSCHRITTEN!", result);
     }
 }
